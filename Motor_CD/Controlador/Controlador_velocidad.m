fcdaddfd9ecae39afb4d9ab49d6e65d6e2dcf8d7
Sofia Acerbi <sofiacerbi00@gmail.com>
2021-08-18 00:35:10 +0200

    Se añaden comentarios

--- Motor_CD/Controlador/Controlador_velocidad.m
+++ Motor_CD/Controlador/Controlador_velocidad.m
@@ -1,10 +1,13 @@
+%% Definición de variable y de función de transferencia
 s = tf('s');
 MotorCD = 14.87/(s+10.4);
 
-%controlSystemDesigner(MotorCD)
+%% Diseño del controlador, descomentar a conveniencia
+controlSystemDesigner(MotorCD)
 %sisotool('rlocus',MotorCD)
 
+%% Definición del modelo paralelo
 CC = tf(C)
 num = cell2mat(CC.numerator);
 den = cell2mat(CC.denominator);
-[ki,~,kp] = residue(num,den)
+[ki,~,kp] = residue(num,den) %variables del modelo paralelo
