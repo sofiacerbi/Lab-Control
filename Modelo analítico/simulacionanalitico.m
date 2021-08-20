f85320c32bcdd783f4e8d6d7cb675cd53648caaf
salorq97 <79665416+salorq97@users.noreply.github.com>
2021-08-19 22:53:51 -0600

    Comentarios varios
    Se describe el script y agrega la forma de simular la respuesta al impulso

--- Modelo analítico/simulacionanalitico.m
+++ Modelo analítico/simulacionanalitico.m
@@ -1,3 +1,4 @@
+%Declacarión de las constantes a utilizar
 Ra=0.5;
 La=0.0015;
 kb=0.03;
@@ -12,11 +13,20 @@ mt=4.5;
 L1=0.4;
 g=9.81;
 beta=0.01;
+
+%Sustituciones necesarias para simplificación
 alpha=kt*kb/mt*L1^2;
 delta=g*(m1-m3)/mt*L1;
+
+%Definición del sistema matricial de las ecuaciones de espacio de estado
 A=[-Ra/La -kb/La 0 0; km/Jm -Bm/Jm 0 0; 0 0 0 1; 0 alpha delta -beta/(mt*L1)];
 B=[1/La; 0;0;0];
 C=[0 0 1 0];
 D=[0];
+
+%Creación de nuestra repretación matricial
 sysss=ss(A, B, C, D);
+
+%Simulación para obtener la respuesta ante el escalón e impulso
 step(sysss);
+%%%impulse(sysss)
