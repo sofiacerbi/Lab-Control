fcd2630c70bbdad47c4913c0dce6c39294992ada
Sofia Acerbi <sofiacerbi00@gmail.com>
2021-11-07 14:26:05 +0100

    Se aumentan las matrices + se crea controlador LQR + se crea controlador UP

---
+++ Heli2DoF/REI/REI.m
@@ -0,0 +1,30 @@
+%% Definición de variables de estado
+A=[0 0 1 0; 0 0 0 1;-1.554 0 -0.7304 0;0 0 0 -0.06447];
+B=[0.03558 0;0.011 0;0.02464 0;0 -0.049458];
+C=[1 0 0 0;0 1 0 0];
+
+%% Variables sistema aumentado
+As = [A [0;0;0;0] [0;0;0;0]; -C [0 0; 0 0]];
+Bs = [B; 0 0; 0 0];
+
+%% Por LQR
+Q = eye(6);
+Q(1,1) = 100;
+Q(2,2) = 100;
+Q(3,3) = 100;
+Q(4,4) = 100;
+Q(5,5) = 1900;
+Q(6,6) = 100;
+R = 1;
+
+KsLQR = lqr(As, Bs, Q, R);
+KLQR = KsLQR(:,1:4)
+KiLQR = -KsLQR(:,5:6)
+eig(As-Bs*KsLQR);
+
+%% Por ubicación de polos
+P = [-0.541+0.1i -0.541-0.1i -2 -2.5 -3 -3.5];
+
+KsUP = place(As, Bs, P);
+KUP = KsUP(:,1:4)
+KiUP = -KsUP(:,5:6)
